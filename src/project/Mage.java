555400c1aa51497804c3b99043c7e59d32b51163
it62070096 <56445826+it62070096@users.noreply.github.com>
2020-10-12 22:27:29 +0700

    Add character

---
+++ src/project/Mage.java
@@ -0,0 +1,23 @@
+package project;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Mage extends Character{
+
+    public Mage() {
+        super(450, 700, 62, 65, 62);
+        super.setPic(new ImageIcon(getClass().getResource("img/mage.gif")).getImage());
+    }
+    
+    public void skill1() {
+        
+    }
+    public void skill2() {
+        
+    }
+    public void skill3() {
+        
+    }
+    
+}
