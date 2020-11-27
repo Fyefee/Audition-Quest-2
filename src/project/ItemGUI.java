6a8a91e4c842edf81c89ffd219c21fe75da02854
TeerapatEarth <72504364+TeerapatEarth@users.noreply.github.com>
2020-11-27 22:03:21 +0700

    Add files via upload

--- src/project/ItemGUI.java
+++ src/project/ItemGUI.java
@@ -1,13 +1,27 @@
 package item;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-public class ItemGUI implements ActionListener{
+import java.util.ArrayList;
+
+public class ItemGUI implements ActionListener {
+
     private JFrame fr;
     private JPanel panel;
     private JButton item1, item2, item3;
-    private JLabel head, space1, space2; 
-    public ItemGUI(){
+    private JLabel head, space1, space2;
+    private ArrayList<String> itemstat = new ArrayList();
+    private int indexItem1, indexItem2, indexItem3;
+
+    public ItemGUI() {
+        itemstat.add("Atk + 2");
+        itemstat.add("Def + 2");
+        itemstat.add("Speed + 50");
+        itemstat.add("Hp + 100");
+        indexItem1 = (int) ((Math.random() * (3 - 0)) + 0);
+        indexItem2 = (int) ((Math.random() * (3 - 0)) + 0);
+        indexItem3 = (int) ((Math.random() * (3 - 0)) + 0);
         fr = new JFrame();
         panel = new JPanel();
         item1 = new JButton("Item 1");
@@ -33,29 +47,33 @@ public class ItemGUI implements ActionListener{
         fr.add(head, BorderLayout.NORTH);
         fr.add(panel);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fr.setSize(650,450);
+        fr.setSize(650, 450);
         fr.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if(ae.getSource().equals(item1)){
+        if (ae.getSource().equals(item1)) {
             item2.setVisible(false);
             item3.setVisible(false);
             panel.remove(item2);
             panel.remove(item3);
-        }
-        else if (ae.getSource().equals(item2)){
+            JOptionPane.showMessageDialog(fr, itemstat.get(indexItem1));
+            fr.dispose();
+        } else if (ae.getSource().equals(item2)) {
             item1.setVisible(false);
             item3.setVisible(false);
             panel.remove(item1);
             panel.remove(item3);
-        }
-        else if (ae.getSource().equals(item3)){
+            JOptionPane.showMessageDialog(fr, itemstat.get(indexItem2));
+            fr.dispose();
+        } else if (ae.getSource().equals(item3)) {
             item1.setVisible(false);
             item2.setVisible(false);
             panel.remove(item1);
             panel.remove(item2);
+            JOptionPane.showMessageDialog(fr, itemstat.get(indexItem3));
+            fr.dispose();
         }
     }
 }
